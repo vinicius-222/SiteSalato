7ae8325d25c61e14441bcfdf1411bb794e353e32
Vinicius Guimaraes <vinicius@salato.com.br>
2020-04-25 17:09:40 -0300

    Inserindo projeto de Delivery

---
+++ build/precache-manifest.486110bc433adffe4d91b7d168d1fd44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932c04c223d5bb20ecbc269b11c2dc88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22da031dd043eebe183",
+    "url": "/static/css/2.47fb9b64.chunk.css"
+  },
+  {
+    "revision": "4bce017780407a8ef64d",
+    "url": "/static/css/main.84379cb3.chunk.css"
+  },
+  {
+    "revision": "e22da031dd043eebe183",
+    "url": "/static/js/2.e3f4201d.chunk.js"
+  },
+  {
+    "revision": "4bce017780407a8ef64d",
+    "url": "/static/js/main.fb472b44.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "97da2b1474e965a1a375dc86c0930273",
+    "url": "/static/media/Logo.97da2b14.png"
+  },
+  {
+    "revision": "1d93ed4e2461a5f50ba177566d23d57e",
+    "url": "/static/media/facebook.1d93ed4e.png"
+  },
+  {
+    "revision": "aa9b851ff1a65324ff28039e02059c2c",
+    "url": "/static/media/home.aa9b851f.png"
+  },
+  {
+    "revision": "cfaf4397964554a5e28e4c3416e66233",
+    "url": "/static/media/instagram.cfaf4397.png"
+  }
+]);
