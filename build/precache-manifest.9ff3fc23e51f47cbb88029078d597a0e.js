45846e1189a98fdeea099125a1a167a36db2013c
Vinicius Guimaraes de Oliveira <vinicius@salato.com.br>
2021-02-20 17:05:53 -0300

    varias altecaoes

---
+++ build/precache-manifest.9ff3fc23e51f47cbb88029078d597a0e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4998dcd8c366caf1ee0b105fd1bbca84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e89ae20fb3d223efb5f",
+    "url": "/static/css/2.1b112e9f.chunk.css"
+  },
+  {
+    "revision": "1c6285fc715150667737",
+    "url": "/static/css/main.320922bc.chunk.css"
+  },
+  {
+    "revision": "8e89ae20fb3d223efb5f",
+    "url": "/static/js/2.d918d58d.chunk.js"
+  },
+  {
+    "revision": "1c6285fc715150667737",
+    "url": "/static/js/main.5ab6329b.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "97da2b1474e965a1a375dc86c0930273",
+    "url": "/static/media/Logo.97da2b14.png"
+  },
+  {
+    "revision": "dc67f52fbb48b7dfde82ee485e22dc5b",
+    "url": "/static/media/bariol-bold-webfont.dc67f52f.ttf"
+  },
+  {
+    "revision": "256143b2519c9b24181fe8172a9884d5",
+    "url": "/static/media/bariol-regular-webfont.256143b2.ttf"
+  },
+  {
+    "revision": "4880430bbf179cdcda3c6d2152cd683a",
+    "url": "/static/media/email.4880430b.png"
+  },
+  {
+    "revision": "1d93ed4e2461a5f50ba177566d23d57e",
+    "url": "/static/media/facebook.1d93ed4e.png"
+  },
+  {
+    "revision": "aa9b851ff1a65324ff28039e02059c2c",
+    "url": "/static/media/home.aa9b851f.png"
+  },
+  {
+    "revision": "cfaf4397964554a5e28e4c3416e66233",
+    "url": "/static/media/instagram.cfaf4397.png"
+  },
+  {
+    "revision": "47bb2642313bd745514d41a82fd726d9",
+    "url": "/static/media/telefone-icone-png.47bb2642.png"
+  },
+  {
+    "revision": "64940ed8e5f28c766536a78f3b8fe6c1",
+    "url": "/static/media/whatsApp.64940ed8.png"
+  }
+]);
